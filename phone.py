510f9eac9234939f70f68fbb5acc88910e737536
Ярослав <113538149+GrandPLAY@users.noreply.github.com>
2023-01-28 12:51:37 +0300

    Контрольная

--- phone.py
+++ phone.py
@@ -1,30 +1,26 @@
-t = 11 * 60
-dt = 15
-s = 10
+plata = 0
+t = 7 * 60 + 45
+dt = 237
+s = 100
 d = 6
 
 t2 = t + dt
 
-if t2 >= 24:
-    d += (t + dt / 60) // 24
-    while d > 7:
-        d = d - 7
-
-# Классические случаи
-if d >= 6:
-    if t < 8 or t >= 22:
-        plata = s * dt * 0.7
-    else:
-        plata = s * dt * 0.9
-else:
-    if t < 8 or t >= 22:
-        plata = s * dt * 0.8
-
 for t in range(t2):
-    if t < 8*60 or t >= 22*60:
-        plata += s * 0.8
-    plata += s
+    print (d)
+    if d >= 6:
+        if t < 8*60 or t >= 22*60:
+            plata += s * 0.7
+        else:
+            plata += s * 0.9
+    else:
+        if t < 8*60 or t >= 22*60:
+            plata += s * 0.8
+        else:
+            plata += s
     t = t + 1
-
+    if t > 24 * 60:
+        d += 1
+        t -= 24 * 60
+        t2 -= 24 * 60
 print(plata)
-
