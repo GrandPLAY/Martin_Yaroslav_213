141065c00d664bb2f81307a2ce36687aa060d9e4
Ярослав <113538149+GrandPLAY@users.noreply.github.com>
2023-01-28 15:15:06 +0300

    Контрольная

--- phone.py
+++ phone.py
@@ -1,16 +1,14 @@
 plata = 0
-t = 7 * 60 + 45
-dt = 237
-s = 100
-d = 6
+t = int(input("Начало разговора - часов: ")) * 60 + int(input("Начало разговора - минут: "))
+dt = int(input("Время разговора (мин): "))
+s = int(input("Тариф (руб/мин): "))
+d = int(input("День недели (порядковым числом): "))
 
 t2 = t + dt
-
-for t in range(t2):
-    print (d)
+while t != t2:
     if d >= 6:
         if t < 8*60 or t >= 22*60:
-            plata += s * 0.7
+            plata += s * 0.72
         else:
             plata += s * 0.9
     else:
@@ -19,8 +17,11 @@ for t in range(t2):
         else:
             plata += s
     t = t + 1
-    if t > 24 * 60:
-        d += 1
-        t -= 24 * 60
-        t2 -= 24 * 60
-print(plata)
+    if t >= 24 * 60:
+    	if d == 7:
+    		d = 1
+    	else:
+    		d += 1
+    	t -= 24 * 60
+    	t2 -= 24 * 60
+print("Итоговая сумма: " + str(plata))
