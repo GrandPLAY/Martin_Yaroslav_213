86573fdcc09a5fd16ff7fc6223986b0286d2a24c
Ярослав <113538149+GrandPLAY@users.noreply.github.com>
2023-02-10 20:02:59 +0300

    Update zodiak.py

--- zodiak.py
+++ zodiak.py
@@ -1,27 +1,27 @@
 day = int(input("День: "))
 month = int(input("Месяц: "))
 
-if day >= 24:
+if day >= 24 and 8 <= month <= 10:
     match month:
         case 8: zodiak = "Дева"
         case 9: zodiak = "Весы"
         case 10: zodiak = "Скорпион"
-elif day >= 23:
+elif day >= 23 and (month == 7 or month == 11):
     match month:
         case 7: zodiak = "Лев"
         case 11: zodiak = "Стрелец"
-elif day >= 22:
+elif day >= 22 and (month == 6 or month == 12):
     match month:
         case 6: zodiak = "Рак"
         case 12: zodiak = "Козерог"
-elif day >= 21:
+elif day >= 21 and 1 <= month <= 5:
     match month:
         case 1: zodiak = "Водолей"
         case 3: zodiak = "Овен"
         case 4: zodiak = "Телец"
         case 5: zodiak = "Близнецы"
-elif day >= 19:
-    if month == 2: zodiak = "Рыбы"
+elif day >= 19 and month == 2:
+    zodiak = "Рыбы"
 else:
     match month:
         case 1: zodiak = "Козерог"
@@ -36,4 +36,4 @@ else:
         case 10: zodiak = "Весы"
         case 11: zodiak = "Скорпион"
         case 12: zodiak = "Стрелец"
-print(zodiak)
+print("Знак зодиака: " + zodiak)
